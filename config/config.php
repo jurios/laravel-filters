a1d0b5bfc74f29c9511b586970727955b0814e7a
Jordi <jordi@kodilab.com>
2019-02-09 09:43:46 +0100

    Improve config comments

--- config/config.php
+++ config/config.php
@@ -6,9 +6,10 @@ return [
     | Filter prefix
     |--------------------------------------------------------------------------
     |
-    | This value is the prefix that your filters will have in your URL request.
-    | If a filter doesn't start with this prefix will be ignored by laravel-filters
+    | You can define a global prefix that your filters will have in the query parameters URL in order to
+    | detect which query parameters are filters and which aren't. This prefix can be also defined once you instances
+    | the filter class and only take effect during the class instance.
     |
     */
-    'prefix' => 'qf'
+    'prefix' => null
 ];
