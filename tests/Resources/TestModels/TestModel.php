1549161118c4f30dc919079638bcbe8e85d04135
Jordi <jordi@kodilab.com>
2019-02-10 23:04:34 +0100

    Add tests

---
+++ tests/Resources/TestModels/TestModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kodilab\LaravelFilters\Tests\Resources\TestModels;
+
+use Illuminate\Database\Eloquent\Model;
+use Kodilab\LaravelFilters\Filterable;
+
+class TestModel extends Model
+{
+    use Filterable;
+
+    protected $table = 'test_models';
+}
